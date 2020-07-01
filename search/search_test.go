252484ca47a9114aadaa5c49d3a5004db9dbeb06
LXie-Saintex <admin@saintex.co.uk>
2020-07-01 13:50:41 +0100

    added failing search test

--- search/search_test.go
+++ search/search_test.go
@@ -1,9 +1,20 @@
 package search
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestSearch_should_handle_routes_that_contain_the_search_param(t *testing.T) {
+func TestSearch_should_send_request_to_mw_api(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/search/boo", nil)
+	Search(w, req)
 
+	res := w.Result()
+	expectedType := "application/json; charset=utf-8"
+
+	if got := res.Header.Get("Content-Type"); got != expectedType {
+		t.Errorf("\nunexpected response type from server: \nexpect: %v\ngot: %v", expectedType, got)
+	}
 }
