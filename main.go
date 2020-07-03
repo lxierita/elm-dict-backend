7e504a71037fb322d12c082d61fee874d9edeabb
LXie-Saintex <admin@saintex.co.uk>
2020-07-03 15:18:58 +0100

    added handler wrapper

--- main.go
+++ main.go
@@ -11,18 +11,7 @@ import (
 
 func main() {
 	router := httprouter.New()
-	router.HandleOPTIONS = true
-	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		header := w.Header()
-		header.Set("Access-Control-Allow-Methods", "GET")
-		header.Set("Access-Control-Allow-Origin", "127.0.0.1:8000")
-
-		// Adjust status code to 204
-		w.WriteHeader(http.StatusNoContent)
-	})
-
 	routing.ConfigRoutes(router)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- router/router.go
+++ router/router.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	"fmt"
-	handlers "lxierita/elm-dict/search"
+	s "lxierita/elm-dict/search"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -16,5 +16,10 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 //ConfigRoutes configures the routes and handlers of the app
 func ConfigRoutes(r *httprouter.Router) {
 	r.GET("/", Index)
-	r.Handler("GET", "/search/:word", http.HandlerFunc(handlers.Search))
+	r.Handler("GET", "/search/:word", http.HandlerFunc(allowAll))
+}
+
+func allowAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	s.Search(w, r)
 }
