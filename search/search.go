2521ad22677477b1cba66934afe49d7c4aa94a7a
LXie-Saintex <admin@saintex.co.uk>
2020-07-05 15:54:00 +0100

    malfored http response

--- search/search.go
+++ search/search.go
@@ -2,6 +2,8 @@ package search
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -17,11 +19,29 @@ const APIKey = "24375962-78c5-4fbc-a585-b37ed4088caf"
 func Search(w http.ResponseWriter, r *http.Request) {
 	ps := httprouter.ParamsFromContext(r.Context())
 
+	r.URL.Scheme = "https"
 	r.URL.Host = "dictionaryapi.com"
 	r.URL.Path = fmt.Sprintf("%v%v?key=%v", MWEndpoint, ps.ByName("word"), APIKey)
 
-	ts := &http.Transport{}
-	ts.Proxy(r)
+	ts := &http.Transport{
+		Proxy: http.ProxyURL(r.URL),
+	}
+	fmt.Println(r.URL)
+	fmt.Println(r.Method)
+	res, err := ts.RoundTrip(r)
+	if err != nil {
+		log.Fatalf("RoundTrip failed: \n%v", err)
+	}
+	body, err1 := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err1 != nil {
+		log.Fatalf("Couldn't read response: \n %v", err1)
+	}
 
 	w.WriteHeader(http.StatusOK)
+	_, err2 := w.Write(body)
+	if err2 != nil {
+		log.Fatalf("ResponseWriter failed to write: \n %v", err2)
+	}
+	ts.CloseIdleConnections()
 }
