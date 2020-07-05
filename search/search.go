e741fc9b2d805fed2b88a7a976536d78c068adfc
LXie-Saintex <admin@saintex.co.uk>
2020-07-05 16:04:11 +0100

    useless api

--- search/search.go
+++ search/search.go
@@ -1,7 +1,6 @@
 package search
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -16,7 +15,9 @@ const APIKey = "24375962-78c5-4fbc-a585-b37ed4088caf"
 //Search writes and sends request to 3rd party API based on given params
 func Search(w http.ResponseWriter, r *http.Request) {
 	ps := httprouter.ParamsFromContext(r.Context())
-	url := fmt.Sprintf("%v%v?key=%v", MWEndpoint, ps.ByName("word"), APIKey)
 
-	fmt.Fprintf(w, "hello, %s!\n", url)
+	test := []byte(ps.ByName("word"))
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(test)
 }
